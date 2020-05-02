d52d1a97150751b09ac01140fda3298070095be4
2paketa <manosprk@gmail.com>
2020-05-02 10:28:25 +0300

    added env variable for dev

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 import { socketEvents } from "./events";
-export const socket = io.connect("192.168.1.8:1337/");
+export const socket = io.connect(process.env.REACT_APP_API_URL || "https://ttboom-web-api-server.herokuapp.com/");
 
 export const initSockets = ({ setValue }) => {
     socketEvents({ setValue });
